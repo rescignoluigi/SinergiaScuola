09a1415513bb767c3639ce8c885b0a708d07c181
rescignoluigi <63789224+rescignoluigi@users.noreply.github.com>
2026-03-26 18:41:53 +0100

    Add Navbar component with navigation links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar-title">SinergiaScuola</div>
+      <nav className="navbar-links">
+        <NavLink to="/plessi" className="nav-link">Plessi</NavLink>
+        <NavLink to="/classi" className="nav-link">Classi</NavLink>
+        <NavLink to="/docenti" className="nav-link">Docenti</NavLink>
+        <NavLink to="/orario" className="nav-link">Orario</NavLink>
+        <NavLink to="/assenze" className="nav-link">Assenze</NavLink>
+        <NavLink to="/disponibilita" className="nav-link">Disponibilità</NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
 
